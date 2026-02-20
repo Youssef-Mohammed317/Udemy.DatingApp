4493fec7318544c3d77e302e252474476fa8e926
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2026-02-20 22:06:54 +0200

    End of Section 3

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -11,11 +11,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowSpecificOrigin", policy =>
+  {
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+  });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowSpecificOrigin");
+
 app.MapControllers();
 
 app.Run();
